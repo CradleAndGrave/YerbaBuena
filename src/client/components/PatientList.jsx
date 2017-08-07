f7de22d9f098fdb1e2c153cf761cdf3ae28bf79f
Timothy Roy <roy.tim@gmail.com>
2017-08-07 13:33:32 -0500

    Conforms to somewhat confusing linter, so will pass TravisCI

--- src/client/components/PatientList.jsx
+++ src/client/components/PatientList.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import Patient from './Patient.jsx'
+import PropTypes from 'prop-types';
+import Patient from './Patient';
 
-//TODO: update call to redux store to populate fields
+// TODO: update call to redux store to populate fields
 
-const Patients = (props) => (
-
+const Patients = props => (
   <div>
-    {props.patients.map((patient, index) =>
-      <div key={ index }><Patient key={ patient.id } patient={ patient } /></div>
+    {props.patients.map(patient =>
+      <div key={ patient.id }><Patient key={ patient.id } patient={ patient } /></div>
     )}
   </div>
 );
+
+Patients.propTypes = {
+  patients: PropTypes.arrayOf(PropTypes.shape).isRequired,
+};
 
 
 export default Patients;
