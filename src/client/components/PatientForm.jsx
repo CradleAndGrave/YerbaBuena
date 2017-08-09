40bdd8f575e8c9f7da6bd3a67554b67b917d0814
Jesus Esquivel <JAER@Jesuss-MacBook-Air.local>
2017-08-09 17:14:29 -0500

    Added Schemas for the APP and added the connection bewteen the server and DB. Also have the SignUp/ButtonInstance/ProviderForm/PatientForm components will work on connecting it to store

---
+++ src/client/components/PatientForm.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PatientForm = () => (
+
+  <form>
+    <input type="text" name="username" />
+    <br />
+    <input type="text" name="password" />
+    <br />
+    <input type="text" name="birhtday" />
+    <br />
+    <input type="text" name="sex" />
+    <br />
+    <input type="submit" />
+  </form>
+);
+
+PatientForm.propTypes = {
+  patientform: PropTypes.arrayOf(PropTypes.shape).isRequired,
+};
+
+export default PatientForm;
---
+++ src/server/db/models/treatmentHistory.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import passportLocalMongoose from 'passport-local-mongoose';
+
+const Schema = mongoose.Schema;
+
+const TreatmentHistorySchema = new Schema({
+  userId: Number,
+  action: [Number],
+  treatName: String,
+  treatDose: String,
+  treatNote: String,
+  actionTime: String
+});
+const History = mongoose.model('Treatment', TreatmentHistorySchema);
+
+TreatmentHistorySchema.plugin(passportLocalMongoose);
+
+export default History;
